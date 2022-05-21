7f9f8e8173cb2f1323aefa6dbaf9b9ebcf9c2695
Антон Щерба <Tony.Shcherba@gmail.com>
2022-05-21 21:08:25 +0300

    fix: fix wrong Kb to correct byte

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,7 +6,7 @@ fs.promises.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true}
     if(filename.isFile()) {
       const filenamePath = path.join(__dirname, 'secret-folder', filename.name);
       fs.stat(filenamePath, (err, stats) => {
-        console.log(`${path.basename(filenamePath, path.extname(filenamePath))} - ${path.extname(filenamePath).substring(1)} - ${stats.size/1000}kb`);
+        console.log(`${path.basename(filenamePath, path.extname(filenamePath))} - ${path.extname(filenamePath).substring(1)} - ${stats.size} byte`);
       });
     }
   });
