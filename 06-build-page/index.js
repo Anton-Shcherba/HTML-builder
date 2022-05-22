9d35d8eccda745577ab7d8fa0e1866852ad7a601
Антон Щерба <Tony.Shcherba@gmail.com>
2022-05-22 10:21:49 +0300

    fix: merge styles solution optimization

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -27,10 +27,8 @@ fs.promises.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }).the
   const outputStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
   fs.promises.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}).then(filenames => {
     filenames.forEach(filename => {
-      if(filename.isFile() && path.extname(path.join(__dirname, 'styles', filename.name)) === '.css') {
-        const inputStream = fs.createReadStream(path.join(__dirname, 'styles', filename.name));
-        inputStream.pipe(outputStream);
-      }
+      const filePath = path.join(__dirname, 'styles', filename.name);
+      if(filename.isFile() && path.extname(filePath) === '.css') fs.createReadStream(filePath).pipe(outputStream);
     });
   });
 });
